4175fa11a4015580131c1a77937d34cf3c10afa5
adonis goushchi <amidonis@hotmail.com>
2021-11-10 15:43:53 +0100

    hardd

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+style = ['chinese', 'italian', 'japanese', 'french', 'belgian']
+5.times do
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::IDNumber.croatian_id,
+    category: style.sample
+  )
+  restaurant.save!
+end
